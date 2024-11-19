e8f657c5913d55ebfa16b0eaa0408eab2f55fe91
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-11-19 13:29:08 +0530

    ebasm -> bm

--- Makefile
+++ Makefile
@@ -1,5 +1,18 @@
 CFLAGS=-Wall -Wextra -Wswitch-enum -std=c11 -pedantic
 LIBS=
 
-bm: main.c
-	$(CC) $(CFLAGS) -o bm main.c $(LIB)
+.PHONY: all
+all: ebasm bmi
+
+ebasm: ebasm.c bm.c
+	$(CC) $(CFLAGS) -o ebasm ebasm.c $(LIB)
+
+bmi: bmi.c bm.c
+	$(CC) $(CFLAGS) -o bmi bmi.c $(LIB)
+
+.PHONY: examples
+examples: ./examples/fib.bm
+
+./examples/fib.bm: ./examples/fib.ebasm
+	./ebasm ./examples/fib.ebasm ./examples/fib.bm
+
---
+++ ebasm.c
@@ -0,0 +1,29 @@
+#include "./bm.c"
+
+int main(int argc, char **argv) {
+    // bm_load_program_from_memory(&bm, program, ARRAY_SIZE(program));
+
+    if(argc < 3) {
+	fprintf(stderr, "Usage: ./ebasm <input.ebasm> <output.bm>\n");
+	fprintf(stderr, "ERROR: expected input and output\n");
+	exit(1);
+    }
+
+    const char *input_file_path = argv[1];
+    const char *output_file_path = argv[2];
+
+    // * Read the basm file
+    String_View source = slurp_file(input_file_path);
+
+    // * Interpret the program
+    bm.program_size = bm_translate_source(
+    source,
+    bm.program,
+    BM_PROGRAM_CAPACITY);
+
+    // * Save the executable
+    bm_save_program_to_file(bm.program, bm.program_size, output_file_path);
+		         
+    return 0;
+}
+
