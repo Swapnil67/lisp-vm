f4a7455cbe0db76801f797f1f28b0edb36ed868e
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-11-30 14:46:48 +0530

    Extend native function enviroment of bme

--- src/bme.c
+++ src/bme.c
@@ -45,69 +45,93 @@ static Err bm_print_f64(Bm *bm) {
     return ERR_OK;
 }
 
-int main(int argc, char **argv) {
+static Err bm_print_u64(Bm *bm) {
+    if(bm->stack_size < 1) {
+	return ERR_STACK_UNDERFLOW;
+    }
+    // printf("Print Unsigned Integer\n");
+    printf("%lld\n", bm->stack[bm->stack_size-1].as_u64);
+    bm->stack_size -= 1;
+    return ERR_OK;
+}
+
+static Err bm_print_i64(Bm *bm) {
+    if(bm->stack_size < 1) {
+	return ERR_STACK_UNDERFLOW;
+    }
+    // printf("Print Integer\n");
+    printf("%lld\n", bm->stack[bm->stack_size-1].as_i64);
+    bm->stack_size -= 1;
+    return ERR_OK;
+}
+
+static Err bm_print_ptr(Bm *bm) {
+    if(bm->stack_size < 1) {
+	return ERR_STACK_UNDERFLOW;
+    }
+    // printf("Print Pointer\n");
+    printf("%p\n", bm->stack[bm->stack_size-1].as_ptr);
+    bm->stack_size -= 1;
+    return ERR_OK;
+}
+
+int main(int argc, char **argv)
+{
     const char *program = shift(&argc, &argv);
-    
     const char *input_file_path = NULL;
+    
     int limit = -1;
     int debug = 0;
-
+    
     while (argc > 0) {
 	const char *flag = shift(&argc, &argv);
 	if (strcmp(flag, "-i") == 0) {
-	    if (argc == 0)
-	    {
+	    if (argc == 0) {
 		usage(stderr, program);
 		fprintf(stderr, "ERROR: No argument is provided for flag `%s`\n", flag);
 		exit(1);
 	    }
 	    input_file_path = shift(&argc, &argv);
 	}
-
-	else if (strcmp(flag, "-l") == 0)
-	{
-	    if (argc == 0)
-	    {
+	else if (strcmp(flag, "-l") == 0) {
+	    if (argc == 0) {
 		usage(stderr, program);
 		fprintf(stderr, "ERROR: No argument is provided for flag `%s`\n", flag);
 		exit(1);
 	    }
-	    limit = atoi(shift(&argc, &argv));
-	}
-	else if (strcmp(flag, "-h") == 0)
-	{
+	    limit = atoi(shift(&argc, &argv));    
+	} else if (strcmp(flag, "-h") == 0) {
 	    usage(stdout, program);
 	    exit(0);
-	}
-	else  if(strcmp(flag, "-d") == 0) {
-	    debug = 1;		
-	}	    		    
-	else	    
-	{
-	    usage(stderr, program);		
+	} else if(strcmp(flag, "-d") == 0) {
+	    debug = 1;
+	} else {
+	    usage(stderr, program);
 	    fprintf(stderr, "ERROR: Unknown flag `%s`\n", flag);		
 	    exit(1);		
-	}	    
-    }	
-
-    if(input_file_path == NULL) {	    
-	usage(stderr, program);	    
+	}
+    }
+    
+    if (input_file_path == NULL) {
+	usage(stderr, program);
 	fprintf(stderr, "ERROR: Input was not provided\n");
 	exit(1);	
-    }	
-
+    }
+    
     // * Read the bytecode program from bm file
     bm_load_program_from_file(&bm, input_file_path);
 
     // * Register native functions to bm
-    bm_push_native(&bm, bm_alloc);	
-    bm_push_native(&bm, bm_free);	
-    bm_push_native(&bm, bm_print_f64);	
-    
-    if(!debug) {
+    bm_push_native(&bm, bm_alloc);	  // 0
+    bm_push_native(&bm, bm_free);	  // 1
+    bm_push_native(&bm, bm_print_f64);    // 2
+    bm_push_native(&bm, bm_print_i64);    // 3 
+    bm_push_native(&bm, bm_print_u64);    // 4
+    bm_push_native(&bm, bm_print_ptr);    // 5
+
+    if (!debug) {
 	Err err = bm_execute_program(&bm, limit);
 	// bm_dump_stack(stdout, &bm);
-	
 	if (err != ERR_OK) {
 	    fprintf(stderr, "ERROR: %s\n", err_as_cstr(err));
 	    return 1;
@@ -117,27 +141,22 @@ int main(int argc, char **argv) {
 	while (limit != 0 && !bm.halt) {
 	    bm_dump_stack(stdout, &bm);
 	    printf("Instruction: %s %lld\n",
-		  inst_name(bm.program[bm.ip].type),
-		  bm.program[bm.ip].operand.as_u64
-	    );
-
+	    inst_name(bm.program[bm.ip].type),
+	    bm.program[bm.ip].operand.as_u64);
 	    getchar();
 
-	    
-	    Err err = bm_execute_inst(&bm);	    
+	    Err err = bm_execute_inst(&bm);
 	    // printf("bm->stack_size %lld\n", bm.stack_size);
-
 	    if (err != ERR_OK) {
 		fprintf(stderr, "ERROR: %s\n", err_as_cstr(err));
 		return 1;
 	    }
-	    if(limit > 0) {
+	    if (limit > 0) {
 		--limit;
 	    }
 	}
     }
     return 0;
-    
 }
 
 // ./bme -i ./examples/fib.bm -l 50
