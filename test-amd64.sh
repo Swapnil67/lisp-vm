4ce5e7f3397a9eeb68ca5b759626975892548d76
swapnil67 <swapniladsul017@gmail.com>
2024-12-12 00:53:43 +0530

    Added 123i example to test-amd64.sh file

--- test-amd64.sh
+++ test-amd64.sh
@@ -5,13 +5,7 @@ set -xe
 
 ./build.sh
 
-./basm2amd64 ./examples/halt.basm > halt.asm
+./basm2amd64 ./examples/123i.basm > 123i.asm
 
-nasm -felf64 halt.asm
-ld -o halt halt.o
-
-nasm -felf64 print_i64.asm
-ld -o print_i64 print_i64.o
-
-nasm -felf64 test.asm
-ld -o test test.o
+nasm -felf64 123i.asm
+ld -o 123i 123i.o
