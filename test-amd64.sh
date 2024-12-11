3b3a8c6b3ae718d243c51772b30596b0381ea2c9
swapnil67 <swapniladsul017@gmail.com>
2024-12-12 00:49:45 +0530

    Created test-amd64.sh file

--- test-amd64.sh
+++ test-amd64.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 
 set -xe
@@ -5,5 +6,12 @@ set -xe
 ./build.sh
 
 ./basm2amd64 ./examples/halt.basm > halt.asm
+
 nasm -felf64 halt.asm
 ld -o halt halt.o
+
+nasm -felf64 print_i64.asm
+ld -o print_i64 print_i64.o
+
+nasm -felf64 test.asm
+ld -o test test.o
