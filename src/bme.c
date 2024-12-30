ec7ebd3820190c458bf7faac84b8399dc85daba4
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-12-30 23:13:13 +0530

    Initial print_u64 implementation

--- src/bme.c
+++ src/bme.c
@@ -100,7 +100,7 @@ static Err bm_dump_memory(Bm *bm) {
     return ERR_OK;
 }
 
-static Err bm_write(Bm *bm) {
+static Err native_write(Bm *bm) {
     if(bm->stack_size < 2) {
 	return ERR_STACK_UNDERFLOW;
     }
@@ -176,7 +176,7 @@ int main(int argc, char **argv)
     bm_push_native(&bm, bm_print_u64);    // 4
     bm_push_native(&bm, bm_print_ptr);    // 5
     bm_push_native(&bm, bm_dump_memory);  // 6
-    bm_push_native(&bm, bm_write);        // 7
+    bm_push_native(&bm, native_write);        // 7
 
     if (!debug) {
 	Err err = bm_execute_program(&bm, limit);
