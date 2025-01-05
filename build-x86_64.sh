591a12db205cab8b50bf0cd9cd79900e12b26916
swapnil67 <swapniladsul017@gmail.com>
2025-01-05 17:57:41 +0530

    Add -F dwarf flags to nasm

--- build-x86_64.sh
+++ build-x86_64.sh
@@ -6,10 +6,10 @@ set -xe
 ./build.sh
 
 ./build/bin/basm2amd64 ./examples/fib.basm > ./build/examples/fib.asm
-nasm -felf64 -g ./build/examples/fib.asm -o ./build/examples/fib.o
+nasm -felf64 -F dwarf -g ./build/examples/fib.asm -o ./build/examples/fib.o
 ld -o ./build/examples/fib.exe ./build/examples/fib.o
 
 ./build/bin/basm2amd64 ./examples/123i.basm > ./build/examples/123i.asm
-nasm -felf64 -g ./build/examples/123i.asm -o ./build/examples/123i.o
+nasm -felf64 -F dwarf -g ./build/examples/123i.asm -o ./build/examples/123i.o
 ld -o ./build/examples/123i.exe ./build/examples/123i.o
 
