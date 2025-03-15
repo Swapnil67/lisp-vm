fe1b940cef6d3ec300eae9d2af1b67834ee4ff3e
swapnil67 <swapniladsul017@gmail.com>
2025-03-15 12:41:15 +0530

    Added more windows shit

--- build.h
+++ build.h
@@ -8,12 +8,11 @@
 #ifdef _WIN32
 #    define WIN32_LEAN_AND_MEAN
 #    include "windows.h"
-#    include "process.h"
+#    include "<process.h>"
 #else
 #    include <unistd.h>
 #    include <sys/stat.h>
 #    include <sys/wait.h>
-#    include <dirent.h>
 #endif // _WIN32
 
 
@@ -22,6 +21,78 @@
 #else
 #    define PATH_SEP "/"
 #endif // _WIN32 
+
+//////////////////////////////
+
+#ifdef _WIN32
+
+struct dirent {
+    char d_name[MAX_PATH + 1];
+};
+
+typedef struct {
+    HANDLE hFind;
+    WIN32_FIND_DAT data;
+    struct dirent *dirent;
+} DIR;
+
+DIR *opendir(const char *dirpath) {
+    assert(dirpath);
+
+    char buffer[MAX_PATH];
+    snprintf(buffer, MAX_PATH, "%s\\*", dirpath);
+
+    DIR *dir = (DIR*)calloc(1, sizeof(DIR));
+
+    dir->hFind = FindFirstFile(buffer, &dir->data);
+    if(dir->hFind == INVALID_HANDLE_VALUE) {
+	goto fail;
+    }
+    return dir;
+
+fail:
+    if(dir) {
+	free(dir);
+    }
+
+    return NULL;
+}
+
+struct dirent *readdir(DIR *dirp) {
+    assert(dirp);
+    if(dirp->dirent) == NULL) {
+	dirp->dirent = (struct dirent*)calloc(1, sizeof(struct dirent));
+    } else {
+	if(!FindFirstFile(dirp->hFind, &dirp->data)) {
+	    return NULL;
+	}
+    }
+
+    memset(dirp->dirent->d_name, 0, sizeof(dirp->dirent->d_name));
+
+    strncpy(
+    dirp->dirent->d_name,
+    dirp->data.cFileName,
+    sizeof(dirp->dirent->d_name) - 1);
+    
+   return dirp->dirent; 
+}
+
+void closedir(DIR *dirp) {
+    assert(dirp);
+
+    FindClose(dirp->hFind);
+    if(dirp->dirent) {
+	free(dirp->dirent);
+    }
+    free(dirp);
+}
+
+#else
+#    include <dirent.h>
+#endif // _WIN32
+
+/////////////////////////////
 
 #define PATH_SEP_SIZE (sizeof(PATH_SEP) - 1)
 
@@ -186,7 +257,7 @@ const char *concat_impl(int ignore, ...) {
 
 #ifdef _WIN32
 void build_h_exec(const char *argv[]) {
-    if(_execvp(argv[0], (char * const*)argv)) {
+    if(_spwanvp(_P_WAIT, argv[0], (char * const*)argv)) {
 	fprintf(stderr, "[ERROR] could not execute child process: %s\n", strerror(errno));
 	exit(1);
     }
@@ -214,7 +285,8 @@ void build_h_exec(const char *argv[]) {
 void cmd_impl(int ignore, ...) {
 
     size_t argc = 0;
-    
+
+    // * Count no. of args
     va_list args;
     FOREACH_VARGS(ignore, arg, args, {
 	// printf("Argv: %s\n", arg);
@@ -226,11 +298,14 @@ void cmd_impl(int ignore, ...) {
     // sizeof(const char *) = 8
     const char **argv = malloc(sizeof(const char *) * (argc + 1));
 
+    // * This will build our executable command
+    // * eg: /build/bin/basm /example/foo.basm /example/foo.bm
     argc = 0;    
     FOREACH_VARGS(ignore, arg, args, {
 	// printf("Argv: %s\n", arg);	
 	argv[argc++] = arg;
     });
+    argv[argc] = NULL;
 
     assert(argc >= 1);
     build_h_exec(argv);
